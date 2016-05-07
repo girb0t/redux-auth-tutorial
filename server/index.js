2522a48cfa883a79e9034bc7b138e3a503db70c5
Pei Xiong Liu <pliu@fiksu.com>
2016-05-06 22:32:30 -0400

    Set up express app

--- server/index.js
+++ server/index.js
@@ -4,11 +4,13 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
-// App Setup
 
+// App Setup
+app.use(morgan('combined')); //for logging incoming requests
+app.use(bodyParser.json({ type: '*/*' })); //any incoming requests will be parsed as json
 
 // Server Setup
 const port = process.env.PORT || 8080;
 const server = http.createServer(app);
 server.listen(port);
-console.log('Serer listening on: ', port);
+console.log('Server listening on: ', port);
