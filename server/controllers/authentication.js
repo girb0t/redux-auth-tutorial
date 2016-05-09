e7681e6c6ba2cc761f8b2313605cfa4905a62ca5
Pei Xiong Liu <peixiong.liu@gmail.com>
2016-05-08 22:46:19 -0400

    Issue webtoken on user create

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,4 +1,14 @@
+const jwt = require('jwt-simple');
 const User = require('../models/user');
+const config = require('../config');
+
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+
+  // convention is that json web tokens have the properties:
+  // sub (subject) and iat (issued-at time)
+  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
 
 exports.signup = function(req, res, next) {
   const email = req.body.email;
@@ -18,7 +28,7 @@ exports.signup = function(req, res, next) {
     const user = new User({ email, password });
     user.save(function(err) {
       if (err) { return next(err); }
-      res.json({ success: true });
+      res.json({ token: tokenForUser(user) });
     });
   });
 };
