9efaba383f80d42a24da59fc10b281f22ea0ec45
Pei Xiong Liu <peixiong.liu@gmail.com>
2016-05-09 23:33:07 -0400

    Create local strategy for passport

--- server/services/passport.js
+++ server/services/passport.js
@@ -3,6 +3,28 @@ const User = require('../models/user');
 const config = require('../config');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+const LocalStrategy = require('passport-local');
+
+// Create local strategy
+const localOptions = { usernameField: 'email'}; //override default 'username' field
+const localLogin = new LocalStrategy(localOptions, function(email, password, done) {
+  User.findOne({ email: email }, function(err, user) {
+    if (err) { return done(err, false); }
+
+    if (user) {
+      user.comparePassword(password, function(err, isMatch) {
+        if (err) { return done(err); }
+        if (isMatch) {
+          return done(null, user);
+        } else {
+          return done(null, false);
+        }
+      });
+    } else {
+      done(null, false);
+    }
+  });
+});
 
 // Set up options for JWT Strategy
 const jwtOptions = {
@@ -28,3 +50,4 @@ const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
 
 // Tell passport to use this strategy
 passport.use(jwtLogin);
+passport.use(localLogin);
