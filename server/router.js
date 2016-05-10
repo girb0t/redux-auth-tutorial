f87dc8a2d3d890a3bf2d7c8076cdd33f34746e52
Pei Xiong Liu <peixiong.liu@gmail.com>
2016-05-09 23:01:25 -0400

    Hook up requireAuth to router

--- server/router.js
+++ server/router.js
@@ -1,5 +1,15 @@
 const Authentication = require('./controllers/authentication');
+const passportService = require('./services/passport');
+const passport = require('passport');
+
+// requireAuth acts as a middleware that is used below
+// This case uses the 'jwt' strategy and does NOT try to create a
+// cookie-based session for the user (since we're using tokens)
+const requireAuth = passport.authenticate('jwt', { session: false });
 
 module.exports = function(app) {
+  app.get('/', requireAuth, function(req, res) {
+    res.send({ hi: 'there' });
+  });
   app.post('/signup', Authentication.signup);
 };
