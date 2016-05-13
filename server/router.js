32600c6a7496b44b6ff07e7e8f34acf3d1773295
Pei Xiong Liu <peixiong.liu@gmail.com>
2016-05-12 23:52:16 -0400

    require signin strategy for /signin route

--- server/router.js
+++ server/router.js
@@ -6,10 +6,12 @@ const passport = require('passport');
 // This case uses the 'jwt' strategy and does NOT try to create a
 // cookie-based session for the user (since we're using tokens)
 const requireAuth = passport.authenticate('jwt', { session: false });
+const requireSignin = passport.authenticate('local', { session: false });
 
 module.exports = function(app) {
   app.get('/', requireAuth, function(req, res) {
     res.send({ hi: 'there' });
   });
+  app.post('/signin', requireSignin, Authentication.signin);
   app.post('/signup', Authentication.signup);
 };
